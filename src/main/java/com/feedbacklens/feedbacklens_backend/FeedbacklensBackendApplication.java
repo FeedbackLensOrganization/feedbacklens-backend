1b18b29e5dd6a578c209d86be3a7bf9e8e55d27c
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-02-18 07:05:59 +0100

    init

---
+++ src/main/java/com/feedbacklens/feedbacklens_backend/FeedbacklensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.feedbacklens.feedbacklens_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbacklensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbacklensBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feedbacklens/feedbacklens_backend/FeedbacklensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedbacklens.feedbacklens_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbacklensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
